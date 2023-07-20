10191998b519bcabd7ac8c99864d88438a4a18dd
Zhihao Su <patricksu3675@gmail.com>
2023-07-20 05:12:10 -0400

    update the negative1 plot to get a spiral shape looking graph

--- negative1-power-x.py
+++ negative1-power-x.py
@@ -4,21 +4,25 @@ from mpl_toolkits.mplot3d import Axes3D
 
 # Define the x range
 x = np.linspace(-2, 2, 400)
-
-# Calculate the real and imaginary parts of (-1)^x using Euler's formula
-y = np.exp(1j * np.pi * x)
-y_real = np.real(y)
-y_imag = np.imag(y)
+# Define the k values
+k_values = [0, 1, 2]
 
 # Create a 3D plot
 fig = plt.figure()
 ax = fig.add_subplot(111, projection="3d")
 
-# Plot the real part
-ax.plot(x, y_real, zs=0, zdir="z", label="Real part of y = (-1)^x")
+# y = (-1)^x = e^( i(2pik + pi) )
+for k in k_values:
+    # Calculate (-1)^x using Euler's formula
+    y = np.exp(1j * (2 * np.pi * k + np.pi) * x)
 
-# Plot the imaginary part
-ax.plot(x, y_imag, zs=0, zdir="y", label="Imaginary part of y = (-1)^x")
+    # Calculate the real and imaginary parts of y
+    y_real = np.real(y)
+    y_imag = np.imag(y)
+
+    # Plot the real and imaginary parts together
+    # Important to plot the
+    ax.plot(x, y_real, y_imag, label=f"y = (-1)^x, k={k}")
 
 # Set labels and title
 ax.set_xlabel("x")
@@ -28,6 +32,7 @@ ax.set_title("3D plot of Real and Imaginary parts of y = (-1)^x")
 
 # Show the legend
 ax.legend()
+plt.savefig("negative1-power-x.png")
 
 # Show the plot
 plt.show()
